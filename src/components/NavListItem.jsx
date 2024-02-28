c6ff98069a4969a7dffcf79f4d9f055ab91c6af2
SIMOHAMMED22 <simofinance22@gmail.com>
2024-02-28 15:13:25 +0100

    all done

--- src/components/NavListItem.jsx
+++ src/components/NavListItem.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './navListItem.css';
 
-function NavListItem ({ nav }) {
+function NavListItem ({ nav, navOnClick }) {
     return (
-        <li><a href={nav.link}>{nav.name}</a>
+        <li>
+           <a
+            href={nav.link}
+            className={`${nav.active ? 'active' : undefined}`}
+            onClick={()=> {
+                navOnClick(nav._id);
+        }}
+        >
+            {nav.name}
+        </a>
         </li>
     );
 }
